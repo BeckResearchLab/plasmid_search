22ac22916d2e3d1144742f557b6c60ea4957c2b1
Dave Beck <dacb@uw.edu>
2017-08-18 11:52:34 -0700

    reduced down to just parse and fixup the table

--- parse_hmmscan_table.py
+++ parse_hmmscan_table.py
@@ -1,16 +1,7 @@
 #!/usr/bin/env python
 
-from __future__ import print_function
-
-import matplotlib
-import matplotlib.pyplot as plt
-from matplotlib.backends.backend_pdf import PdfPages
 import numpy as np
 import pandas as pd
-from sqlalchemy import create_engine
-
-engine = create_engine('mysql://dacb@mysql/plasmid_search')
-conn = engine.connect()
 
 # Clean up the Resfams.tbl file by making it tab delimited and 
 # stripping off the header, footer, and small woodland creatures 
@@ -56,71 +47,14 @@ df = pd.read_csv('Resfams.tsv', sep='\t',
                         "dom" : np.int64, "rep" : np.int64, "inc" : np.int64,
                         "description" : object, })
 
+# Clean up the query field
+def extract_query(s):
+    return s.split("|")[1]
+df["query"] = df["query"].apply(extract_query)
+
 # For each match, find the minimum value using a groupby
 print("finding the minimum evalue for each query sequence")
 df_min_evalue = df.loc[df.groupby("query")["full_evalue"].idxmin()]
 print(df_min_evalue.shape)
-print("saving to sql")
-df_min_evalue.to_sql('hmmscan_min_evalue', conn)
-
-# Read in the catalog, note this is a huge file
-print("reading the refseq catalog")
-organism_table = pd.read_csv('RefSeq-release83.catalog.gz', sep='\t',
-                            names=["taxid", "organism", "id", "gi", "db", 
-                                "status", "length"],
-                            dtype={ "taxid" : np.int64,
-                                "organism" : object,
-                                "id" : object,
-                                "gi" : np.int64,
-                                "db" : object,
-                                "status" : object,
-                                "length" : np.int64 })
-print("discarding any non plasmid entities from the catalog")
-organism_table = organism_table.loc[organism_table["db"].str.contains("plasmid")]
-organism_table['id'] = organism_table['id'].map(lambda x: "ref|{}|".format(x))
-#organism_table.to_csv('organism_table.tsv', sep='\t')
-print("saving to sql")
-organism_table.to_sql('organism_table', conn)
-
-# Read in the lineages table with the taxonomy for each species
-print("reading the taxonomy lineage data from NCBI")
-lineages_table = pd.read_csv('lineages-2017-08-07.csv.gz', low_memory=False)
-lineages_table.to_sql('lineages_table', conn)
-
-# Join the catalog to the hmmscan data
-print("joining the catalog to the hmmscan data")
-#df_min_organism = df_min_evalue.merge(organism_table, how='left', left_on = 'query', right_on = 'id')
-#print(df_min_organism.shape)
-#df_min_organism.to_csv('hmmscan_organism.tsv', sep='\t')
-
-# Join to the lineage file
-print("joining the hmmscan data with organism annotations to the lineage table")
-#df_min_organism_lineages = df_min_organism.merge(lineages_table, how = 'left', left_on = 'organism', right_on = 'species')
-#print(df_min_organism_lineages.shape)
-#df_min_organism_lineages.to_csv('hmmscan_organism_lineage.tsv', sep='\t')
-
-# Process out some of the duplicates
-print("cleaning the table from duplicates")
-final_df = df_min_organism_lineages[
-                df_min_organism_lineages['subspecies'].isnull() & 
-                df_min_organism_lineages['no rank2'].isnull() & 
-                df_min_organism_lineages['no rank1'].isnull()
-            ]
-print(final_df.shape)
-
-# save figures
-with PdfPages('figures.pdf') as pdf:
-    final_df.groupby('family').query.count().plot(kind = "bar")
-    pdf.savefig()
-    plt.close()
-
-    final_df.groupby('genus').query.count().plot(kind = "bar")
-    pdf.savefig()
-    plt.close()
-
-
-# Save out the file
-print("saving the result")
-final_df.to_csv(path_or_buf='final_table.tsv', sep='\t')
-
-conn.close()
+print("saving to tsv")
+df_min_evalue.to_csv('hmmscan_min_evalue.tsv', sep='\t', index=False)
